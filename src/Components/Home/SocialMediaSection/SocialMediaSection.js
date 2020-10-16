b41b371708f98e11f1925d275b2c57e9163c2b52
Maruf Hossain <hossain815265@gmail.com>
2020-10-16 20:19:06 +0600

    everything is done

--- src/Components/Home/SocialMediaSection/SocialMediaSection.js
+++ src/Components/Home/SocialMediaSection/SocialMediaSection.js
@@ -8,20 +8,20 @@ import airbnb from '../../../images/logos/airbnb.png'
 const SocialMediaSection = () => {
     return (
         <div className="text-center mt-5">
-            <a target="_blank" href="https://slack.com/">
-                <img className="social-media-image mx-5 my-3" src={slack} alt="" />
+            <a className="mx-5 my-3" target="_blank" href="https://slack.com/">
+                <img className="social-media-image" src={slack} alt="" />
             </a>
-            <a target="_blank" href="https://www.google.com/">
-                <img className="social-media-image mx-5 my-3" src={google} alt="" />
+            <a className="mx-5 my-3" target="_blank" href="https://www.google.com/">
+                <img className="social-media-image" src={google} alt="" />
             </a>
-            <a target="_blank" href="https://www.uber.com/">
-                <img className="social-media-image mx-5 my-3" src={uber} alt="" />
+            <a className="mx-5 my-3" target="_blank" href="https://www.uber.com/">
+                <img className="social-media-image" src={uber} alt="" />
             </a>
-            <a target="_blank" href="https://www.netflix.com/">
-                <img className="social-media-image mx-5 my-3" src={netflix} alt="" />
+            <a className="mx-5 my-3" target="_blank" href="https://www.netflix.com/">
+                <img className="social-media-image" src={netflix} alt="" />
             </a>
-            <a target="_blank" href="https://www.airbnb.com/">
-                <img className="social-media-image mx-5 my-3" src={airbnb} alt="" />
+            <a className="mx-5 my-3" target="_blank" href="https://www.airbnb.com/">
+                <img className="social-media-image" src={airbnb} alt="" />
             </a>
         </div>
     );
